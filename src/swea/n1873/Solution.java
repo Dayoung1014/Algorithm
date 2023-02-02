483633a7b9658093aedc10c8a32ea59fa963f10d
everyyoung99 <everyyoung99@gmail.com>
2023-02-02 16:16:43 +0900

    [ing] baekjoon.n17136

--- src/swea/n1873/Solution.java
+++ src/swea/n1873/Solution.java
@@ -1,5 +1,9 @@
 package swea.n1873;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 // https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5LyE7KD2ADFAXc#
 
 /*
@@ -24,7 +28,6 @@ L Left : 왼 / 한 칸 왼쪽이 평지라면  그 칸으로 이동
 R Right : 오 / 한 칸 오른쪽이 평지라면  그 칸으로 이동
 S Shoot  : 전차가 바라보는 방향으로 포탄 발사
 
-
 전차 : 맵 밖이라면 이동 X
 발사된 포탄 : 벽돌or강철 벽에 충돌(포탄 소멸) / 맵 밖 나갈 때 까지 직진(아무 일도 없음)
 강철 벽은 남아있음
@@ -44,11 +47,10 @@ import java.util.Scanner;
 
 class Solution
 {
+	
+
 	public static void main(String args[]) throws Exception
-	{
-		char[] rule = {'.', '*', '#', '-'};
-		char[] move = {'U', 'D', 'L', 'R', 'S'};
-		
+	{	
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 		
@@ -57,18 +59,118 @@ class Solution
 			int H = sc.nextInt(); 
 			int W = sc.nextInt();
 			char [][] map = new char[H][W];
-			int[] idx = new int[2]; // 전차의 h w 위치
-			int view = 0; //전차의 방향
+			int trainH = 0; int trainW = 0;
 			
 			for(int h=0; h<H; h++) {
 				String str = sc.next();
 				for(int w=0; w<W; w++) {
 					map[h][w] = str.charAt(w);
 					//전차 위치 저장
-					
+					if(Arrays.asList('^', 'v', '<', '>').contains(map[h][w])) {
+						trainH= h; trainW= w;
+					}
 				}
 			}
 		
+			int user = sc.nextInt();
+			String input = sc.next();
+			for(int i=0; i<user; i++) {
+
+				char now = input.charAt(i);
+				if(now == 'S') { // (* -> . ) (# -> 포탄 멈춤)
+					if(map[trainH][trainW] == '^') { 
+						breakout:
+						for(int j=trainH; j>=0;  j--) {
+							if(map[j][trainW] == '*') {
+								map[j][trainW]  = '.';
+								break breakout;
+							}
+							else if (map[j][trainW] == '#') {
+								break breakout;
+							}
+						}
+					}
+					else if(map[trainH][trainW] ==  'v') {
+						breakout:
+						for(int j=trainH; j<H;  j++) {
+							if(map[j][trainW] == '*') {
+								map[j][trainW]  = '.';
+								break breakout;
+							}
+							else if (map[j][trainW] == '#') {
+								break breakout;
+							}
+						}
+					}
+					else if (map[trainH][trainW] == '<') {
+						breakout:
+						for(int j=trainW; j>=0;  j--) {
+							if(map[trainH][j] == '*') {
+								map[trainH][j]  = '.';
+								break breakout;
+							}
+							else if (map[trainH][j] == '#') {
+								break breakout;
+							}
+						}
+					}
+					else if (map[trainH][trainW] == '>') {
+						breakout:
+						for(int j=trainW; j<W;  j++) {
+							if(map[trainH][j] == '*') {
+								map[trainH][j]  = '.';
+								break breakout;
+							}
+							else if (map[trainH][j] == '#') {
+								break breakout;
+							}
+						}
+					}
+					
+				}
+				else if(now == 'U') {
+					map[trainH][trainW] = '^';
+					if(trainH>=1 && map[trainH-1][trainW]=='.') {
+						map[trainH][trainW] = '.';
+						trainH--;
+						map[trainH][trainW] = '^';
+					}
+					
+				}
+				else if(now == 'D') {
+					map[trainH][trainW] = 'v';
+					if(trainH<H-1 && map[trainH+1][trainW]=='.') {
+						map[trainH][trainW] = '.';
+						trainH++;
+						map[trainH][trainW] = 'v';
+					}
+				}
+				else if(now == 'L') {
+					map[trainH][trainW] = '<';
+					if(trainW>=1 && map[trainH][trainW-1]=='.') {
+						map[trainH][trainW] = '.';
+						trainW--;
+						map[trainH][trainW] = '<';
+					}
+				}
+				else if(now == 'R') {
+					map[trainH][trainW] = '>';
+					if(trainW<W-1 && map[trainH][trainW+1]=='.') {
+						map[trainH][trainW] = '.';
+						trainW++;
+						map[trainH][trainW] = '>';
+					}
+				}
+			}
+			
+			System.out.print("#"+test_case+" ");
+			for(int h=0; h<H; h++) {
+				for(int w=0; w<W; w++) {
+					System.out.print(map[h][w]);
+				}
+				System.out.println();
+			}
+			
 		
 		}
 	}
