bde0eb1bdc3b22aa2fdefcf6d5972d844389b78b
Dayoung <everyyoung99@gmail.com>
2023-02-06 17:50:57 +0900

    230206

---
+++ src/baekjoon/n17478/Main.java
@@ -0,0 +1,16 @@
+package baekjoon.n17478;
+
+import java.util.Scanner;
+
+/*
+ * 재귀함수 자동 응답 챗봇 만들기
+ * */
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in); 
+		int N = sc.nextInt(); //교수님이 출력을 원하는 재귀 횟수
+	}
+
+}
