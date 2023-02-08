a86aec6656aceeae761fe1cb8a47942bd140cd13
Dayoung1014 <58163364+Dayoung1014@users.noreply.github.com>
2023-02-09 00:07:49 +0900

    230208 hw

--- src/baekjoon/n2477/Main2.java
+++ src/baekjoon/n2477/Main2.java
@@ -18,34 +18,34 @@ import java.util.Scanner;
 
 public class Main2 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int K = sc.nextInt(); // 1m^2에서 자라는 참외 개수
-		int x = 0; int y =0;
-		HashMap<Integer, Integer> maxMap = new HashMap<>();
-		HashMap<Integer, ArrayList<Integer>> minMap = new HashMap<>();
-		// 1 2 / 3 4 중 한번씩 나온게 큰 사각형의 가로, 세로
-		// 두 번 나온게 
-		for(int i=0; i<6; i++) {
-			int dir = sc.nextInt();
-			int len = sc.nextInt();
-			if(!maxMap.containsKey(dir)) { // 존재하지 않는다면 
-				maxMap.put(dir, len); // 넣어준다
-			}
-			else { //존재한다면 
-				if(!minMap.containsKey(dir)) {
-					
-					minMap.put(dir, );
-					maxMap.remove(dir);
-				}
-				else {
-					minMap.put(dir, new int[] {minMap.get(minMap));
-					maxMap.remove(dir);
-				}
-				
-			}
-		}
-		
-	}
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int K = sc.nextInt(); // 1m^2에서 자라는 참외 개수
+//		int x = 0; int y =0;
+//		HashMap<Integer, Integer> maxMap = new HashMap<>();
+//		HashMap<Integer, ArrayList<Integer>> minMap = new HashMap<>();
+//		// 1 2 / 3 4 중 한번씩 나온게 큰 사각형의 가로, 세로
+//		// 두 번 나온게
+//		for(int i=0; i<6; i++) {
+//			int dir = sc.nextInt();
+//			int len = sc.nextInt();
+//			if(!maxMap.containsKey(dir)) { // 존재하지 않는다면
+//				maxMap.put(dir, len); // 넣어준다
+//			}
+//			else { //존재한다면
+//				if(!minMap.containsKey(dir)) {
+//
+//					minMap.put(dir, );
+//					maxMap.remove(dir);
+//				}
+//				else {
+//					minMap.put(dir, new int[] {minMap.get(minMap));
+//					maxMap.remove(dir);
+//				}
+//
+//			}
+//		}
+//
+//	}
 
 }
