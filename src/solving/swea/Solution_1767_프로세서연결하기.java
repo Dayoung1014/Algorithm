72208a7731023df4531ca35fd4355b3da3391226
Dayoung <everyyoung99@gmail.com>
2023-03-06 13:33:15 +0900

    Update Solution_1767_프로세서연결하기.java

--- src/solving/swea/Solution_1767_프로세서연결하기.java
+++ src/solving/swea/Solution_1767_프로세서연결하기.java
@@ -1,7 +1,9 @@
 package solving.swea;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.StringTokenizer;
