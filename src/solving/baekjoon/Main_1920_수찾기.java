83508e82e3a7ed69cdfbe9155edaab2c0186d504
Dayoung <58163364+Dayoung1014@users.noreply.github.com>
2023-04-11 13:49:09 +0900

    Update Main_1920_수찾기.java

--- src/solving/baekjoon/Main_1920_수찾기.java
+++ src/solving/baekjoon/Main_1920_수찾기.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+
 public class Main_1920_수찾기 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
