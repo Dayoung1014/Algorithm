6bd03770166f3aa8ae4f40da25413d13d28cdf9e
Dayoung <58163364+Dayoung1014@users.noreply.github.com>
2023-04-11 10:25:12 +0900

    baekjoon 16918 봄버맨

--- src/solving/baekjoon/Main_16918_봄버맨.java
+++ src/solving/baekjoon/Main_16918_봄버맨.java
@@ -3,28 +3,14 @@ package solving.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 /*
- * R*C인 격자 위에 봄버맨 살고 있음
- * 각 칸은 비어있거나 폭탄 들어있음
- * 
- * 폭탄이 있는 칸은 3초 후 폭발
- * 폭탄이 폭발한 이후에는 폭탄 있던 칸 + 인접 네 칸 파괴되어 빈칸 됨
- * 
- * 1. 가장 처음 봄버맨이 폭탄 설치 
- * 2. 1초 기다림
- * 3. 다음 1초 동안 폭탄이 설치되지 않은 모든 칸에 설치
- * 4. 1초가 지남
- * 5. 3초 시점에 폭탄 폭발
- * 
- * 3~4를 반복
- * 
- * N초가 흐른 후 격자판의 상태 출력
- * 빈 칸은 '.'로, 폭탄은 'O'
+ * 주의해야 하는 것
+ * 연쇄 반응은 없다.
+ * -> 반복문을 통해 폭발 시 연쇄 반응 발생
+ * -> 순차적 폭발이 아닌 동시 다발적 폭발 진행
  * */
 
 public class Main_16918_봄버맨 {
@@ -34,13 +20,10 @@ public class Main_16918_봄버맨 {
 	static int[] dc = {0, 0, 0, -1, 1};
 	
 	static class Pos{
-		int r, c, start;
+		int start;
 		boolean isBomb;
 		
-		public Pos(int r, int c, int start, boolean isBomb) {
-			super();
-			this.r = r;
-			this.c = c;
+		public Pos(int start, boolean isBomb) {
 			this.start = start;
 			this.isBomb = isBomb;
 		}
@@ -52,67 +35,59 @@ public class Main_16918_봄버맨 {
 		R = Integer.parseInt(st.nextToken()); //R * C
 		C = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken()); //N초뒤
-		
-		
-		
-		/*
-		 *  * 1. 가장 처음 봄버맨이 폭탄 설치 
-			 * 2. 1초 기다림
-			 * 3. 다음 1초 동안 폭탄이 설치되지 않은 모든 칸에 설치
-			 * 4. 1초가 지남
-			 * 5. 3초 시점에 폭탄 폭발
-			 * 
-			 * 3~4를 반복
-		 * */
-		
 		map = new Pos[R][C];
-		time = 0;
+		
 		//1. 가장 처음 봄버맨이 폭탄 설치 
-		for (int i = 0; i < R; i++) {
+		for (int i = 0; i < R; i++) { 
 			String line = br.readLine();
 			for (int j = 0; j < C; j++) {
-				if(line.charAt(j)=='.') map[i][j] = new Pos(i, j, time, false);
-				else map[i][j] = new Pos(i, j, time, true);
+				if(line.charAt(j)=='.') map[i][j] = new Pos(0, false);
+				else map[i][j] = new Pos(0, true);
 			}
 		}
 		
-		time = 1; //2. 1초 기다림
-		while(time<=N) {
-			time++; // 3. 다음 1초 동안 폭탄이 설치되지 않은 모든 칸에 설치
-			if(time%2==0) {
+		time=1;
+		//2. 1초 기다림
+		while(time++<N) {
+			// 3. 다음 1초 동안 폭탄이 설치되지 않은 모든 칸에 설치
+			if(time%2==0||time==1) { 
 				for (int i = 0; i < R; i++) {
 					for (int j = 0; j < C; j++) {
-						if(!map[i][j].isBomb) map[i][j] = new Pos(i, j, time, true);
+						if(!map[i][j].isBomb) map[i][j] = new Pos(time, true);
 					}
 				}
 			}
 			 
-			else {
+			else { 
+				ArrayList<int[]> list = new ArrayList<>();
 				for (int i = 0; i < R; i++) {
 					for (int j = 0; j < C; j++) {
 						if(map[i][j].isBomb && map[i][j].start+3==time) {
-							
-							for (int d = 0; d < 5; d++) {
-								int nr = i+dr[d];
-								int nc = i+dc[d];
-								if(nr<0 || nc<0 || nr>=R || nc>=C ) continue;
-								map[nr][nr] = new Pos(nr, nr, time, false);
-							}
+							list.add(new int[] {i, j});
 						}
 					}
 				}
+				
+				for(int[] arr : list) {
+					for (int d = 0; d < 5; d++) {
+						int nr = arr[0]+dr[d];
+						int nc = arr[1]+dc[d];
+						if(nr<0 || nc<0 || nr>=R || nc>=C ) continue;
+						map[nr][nc] = new Pos(0, false);
+					}
+				}
+				
 			}
 		}
 		
-		
+		//출력
 		for (int i = 0; i < R; i++) {
 			for (int j = 0; j < C; j++) {
 				if(map[i][j].isBomb) System.out.print("O");
-				else System.out.println(".");
+				else System.out.print(".");
 			}
 			System.out.println();
 		}
-		
-		
+		System.out.println();
 	}
 }
