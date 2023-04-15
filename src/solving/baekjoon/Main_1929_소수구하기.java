f8d739cdeb6bc9a48ed7e8804860613d4bbd3270
Dayoung1014 <58163364+Dayoung1014@users.noreply.github.com>
2023-04-16 00:13:31 +0900

    Create Main_1929_소수구하기.java

---
+++ src/solving/baekjoon/Main_1929_소수구하기.java
@@ -0,0 +1,37 @@
+package solving.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1929_소수구하기 {
+    static boolean[] notPrime;
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+
+        notPrime = new boolean[N+1];
+        getPrime();
+        StringBuilder sb = new StringBuilder();
+        for (int i = M; i <= N ; i++) {
+            if(!notPrime[i]) sb.append(i+"\n");
+        }
+        System.out.println(sb);
+    }
+    static void getPrime() {
+        notPrime[0] = notPrime[1] = true;
+
+        for (int i = 2; i <=Math.sqrt(notPrime.length) ; i++) {
+            for(int j = 2; j<=N/i; j++) {
+                if(notPrime[i*j]==true) continue;
+                else notPrime[i*j] = true;
+            }
+        }
+    }
+}
