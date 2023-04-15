a03f8d8d2243b8fefe99eee0a49ccd6d99407c19
Dayoung1014 <58163364+Dayoung1014@users.noreply.github.com>
2023-04-15 22:43:52 +0900

    Update Main_2156_포도주시식.java

--- src/solving/baekjoon/Main_2156_포도주시식.java
+++ src/solving/baekjoon/Main_2156_포도주시식.java
@@ -3,7 +3,6 @@ package solving.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 /*
  * 선택 시 모두 마시고 원래 위치에 다시 놓아야 함
@@ -13,28 +12,24 @@ import java.util.Arrays;
  * */
 
 public class Main_2156_포도주시식 {
-
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		int[] arr = new int[n];
+		int[] arr = new int[n+1];
 		
-		for (int i = 0; i < arr.length; i++) {
+		for (int i = 1; i < arr.length; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
 		
-		int[] dp = new int[n];
-		for (int i = 0; i < dp.length; i++) {
-			if(i==0) dp[0] = arr[0];
-			else if(i==1) dp[1] = dp[0] + arr[1];
+		int[] dp = new int[n+1];
+		for (int i = 1; i < dp.length; i++) {
+			if(i==1) dp[1] = arr[1];
+			else if(i==2) dp[2] = dp[1] + arr[2];
 			else {
-				// 
-			
+				dp[i] = Math.max(Math.max(dp[i-2] + arr[i] , dp[i-1]), dp[i-3]+arr[i-1]+arr[i]);
 			}
 		}
-		System.out.println(Arrays.toString(arr));
-		System.out.println(Arrays.toString(dp));
-
+		System.out.println(dp[n]);
 	}
 
 }
