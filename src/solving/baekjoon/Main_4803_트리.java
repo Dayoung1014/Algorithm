079aefc2e23022ec18df47ce716b85c93157b003
Dayoung1014 <58163364+Dayoung1014@users.noreply.github.com>
2023-07-01 11:21:41 +0900

    [BOJ] 4803_트리

--- src/solving/baekjoon/Main_4803_트리.java
+++ src/solving/baekjoon/Main_4803_트리.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -15,7 +16,7 @@ import java.util.StringTokenizer;
 
 public class Main_4803_트리 {
     public static void main(String[] args) throws IOException {
-        System.setIn(new FileInputStream("src/upload.acmicpc.net.txt"));
+        //System.setIn(new FileInputStream("src/upload.acmicpc.net.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int test_case = 0;
@@ -47,38 +48,51 @@ public class Main_4803_트리 {
                     //System.out.println(i+" 이미 방문함");
                     continue ;
                 }
-                if(graph[i].size()==0) continue ;
-
-                //System.out.println(tree);
-                v[i] = true;
-
-                Queue<Integer> q = new LinkedList<>();
-                for(int next : graph[i]) {
-                    //System.out.println(i + "에 연결된 것들 방문 " + next);
-                    q.add(next);
-                    v[next] = true;
+                if(graph[i].size()==0) {
+                    //System.out.println("혼자 존재함");
+                    tree++;
+                    continue ;
                 }
 
+                Queue<Integer> q = new LinkedList<>();
+
+                v[i] = true;
+                q.add(i);
+
+                ArrayList<Integer> tmp = new ArrayList<>();
                 while(!q.isEmpty()) {
                     int now = q.poll();
+                    //System.out.println(now + " "+tree);
                     for(int next : graph[now]) {
                         //System.out.println(now + "의 연결된 것들 방문" + next);
                         if(v[next]) {
                             //System.out.println("사이클 생성");
-                            //continue out;
                             circle = true;
                         }
-                        if(!circle) q.add(next);
-                        v[next] = true;
+                        else {
+                            v[next] = true;
+                            q.add(next);
+                        }
                     }
+
+/*                    if(!circle) {
+                        tree++;
+                        for(int next : graph[now]) {
+                            v[next] = true;
+                            q.add(next);
+                        }
+                    }
+                    else {
+                        for(int next : graph[now]) {
+                            v[next] = true;
+                        }
+                    }*/
                 }
-                if(circle) continue out;
-                tree++;
+
+                if(!circle) tree++;
+
             }
 
-            for (int i = 1; i <=n ; i++) {
-                if(!v[i]) tree++;
-            }
 
             System.out.print("Case "+test_case+": ");
             if(tree > 1) System.out.println("A forest of " + tree + " trees.");
