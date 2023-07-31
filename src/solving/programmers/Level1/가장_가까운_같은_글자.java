b44be16a780d069571dd69b7bcca5794bb043120
Dayoung1014 <58163364+Dayoung1014@users.noreply.github.com>
2023-07-31 22:13:12 +0900

    Create 가장_가까운_같은_글자.java

---
+++ src/solving/programmers/Level1/가장_가까운_같은_글자.java
@@ -0,0 +1,28 @@
+package solving.programmers.Level1;
+
+public class 가장_가까운_같은_글자 {
+    public int[] solution(String s) {
+
+        int len = s.length();
+        int[] answer = new int[len];
+        int[] alpha = new int['z'-'a'+1];
+
+        for(int i=0; i<alpha.length; i++) {
+            alpha[i] = -1;
+        }
+
+        for(int i=0; i<s.length(); i++) {
+            char now = s.charAt(i);
+            // 처음 나옴
+            if(alpha[now-'a']==-1) answer[i] = -1;
+
+                // 나온적 있는 경우
+            else answer[i] = i-alpha[now-'a'];
+
+            // 알파벳의 최근 인덱스 갱신
+            alpha[now-'a'] = i;
+        }
+
+        return answer;
+    }
+}
