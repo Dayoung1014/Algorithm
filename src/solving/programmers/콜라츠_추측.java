5facc632c6d05d4a7b3a63f93dd269481b5bf2db
Dayoung1014 <58163364+Dayoung1014@users.noreply.github.com>
2023-08-05 14:35:15 +0900

    Create 콜라츠_추측.java

---
+++ src/solving/programmers/콜라츠_추측.java
@@ -0,0 +1,18 @@
+package solving.programmers.Level1;
+
+public class 콜라츠_추측 {
+    public int solution(int num) {
+        long n = num;
+        int answer = 0;
+        while(n>1) {
+            if(answer==500) {
+                answer=-1;
+                break;
+            }
+            if(n%2==0) n /= 2;
+            else n = n*3 + 1;
+            answer++;
+        }
+        return answer;
+    }
+}
