191544da43edcbd584b268dee8ac957541aaa0d4
Dayoung <58163364+Dayoung1014@users.noreply.github.com>
2023-11-22 15:41:06 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 24.76 ms, Memory: 110 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public ArrayList<Integer> solution(int []arr) {
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        for(int i=1; i<arr.length; i++) {
+        	if(list.get(list.size()-1) != arr[i]) {
+        		list.add(arr[i]);
+        	}
+        }
+        
+        return list;
+    }
+}
