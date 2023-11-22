a2552419461af4ae4bda682d090b483e11ed700d
Dayoung <58163364+Dayoung1014@users.noreply.github.com>
2023-11-22 15:41:10 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+    
+        // n -> 3진법 & 결과는 뒤부터 저장됨
+        ArrayList<Integer> arith = new ArrayList();
+        int now = n;
+        while(true) {
+            if(now == 1) {
+                arith.add(1);
+                break;
+            }
+            
+            int share = now/3;
+            int remain = now%3;
+            
+            arith.add(remain);
+            if(share == 0 || share == 1 || share == 2) {
+                arith.add(share);
+                break;
+            }
+            now = share;
+        }
+        
+        //System.out.println(arith);
+        
+        // 10진법으로 변경
+        for(int i=0; i<arith.size(); i++) {
+            int opt = arith.size()-1-i;
+            answer += arith.get(i) * Math.pow(3, opt);
+        }
+        
+        return answer;
+    }
+}
