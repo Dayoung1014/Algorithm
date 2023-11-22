6e11142f8e06cf4f23985a58868336d01d41b2ef
Dayoung <58163364+Dayoung1014@users.noreply.github.com>
2023-11-22 15:41:20 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 9.61 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+ 
+        int idx = 0;
+        for(int i=0; i<s.length(); i++) {
+            char now = s.charAt(i);
+            if(now==' ') {
+                idx = 0; 
+            }
+            else {
+                if(idx % 2 == 0) now = Character.toUpperCase(now);
+                else now = Character.toLowerCase(now);
+                idx++;
+            }
+            answer += now;
+        }
+        return answer;
+    }
+}
