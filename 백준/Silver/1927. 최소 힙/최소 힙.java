6f82f5351561045b2652bc393f4fe056f63224e7
Dayoung <58163364+Dayoung1014@users.noreply.github.com>
2024-01-08 10:02:16 +0900

    [Silver II] Title: 최소 힙, Time: 1084 ms, Memory: 32224 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+	static PriorityQueue<Number> pq = new PriorityQueue<>();
+	
+	static class Number implements Comparable<Number>{
+		int num;
+
+		
+		public Number(int num) {
+			super();
+			this.num = num;
+		}
+
+
+		@Override
+		public int compareTo(Number o) {
+			return this.num-o.num;
+		}
+
+
+		@Override
+		public String toString() {
+			return Integer.toString(num);
+		}
+		
+		
+	}
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			int order = Integer.parseInt(br.readLine());
+			
+			if(order == 0) { //배열에서 가장 작은 값을 출력하고 그 값을 배열에서 제거
+				if(pq.isEmpty()) System.out.println(0);
+				else System.out.println(pq.poll());
+			}
+			else { //배열에 x라는 값을 넣는(추가하는) 연산
+				pq.offer(new Number(order));
+			}
+		}
+		
+	}
+}
