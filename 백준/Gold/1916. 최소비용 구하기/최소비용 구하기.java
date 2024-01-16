e37a85d3bfbf5fd0075844921fb56c3dfde32d6d
Dayoung <58163364+Dayoung1014@users.noreply.github.com>
2024-01-16 09:52:32 +0900

    [Gold V] Title: 최소비용 구하기, Time: 496 ms, Memory: 51332 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static ArrayList<Bus>[] graph;
+
+    static class Bus implements Comparable<Bus>{
+        int e,c;
+
+        public Bus(int e, int c) {
+            this.e = e;
+            this.c = c;
+        }
+
+        @Override
+        public int compareTo(Bus o) {
+            return this.c-o.c;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine()); //도시의 개수
+        M = Integer.parseInt(br.readLine()); //버스의 개수
+
+        graph = new ArrayList[N+1]; //0비우기
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken()); //버스 출발 도시
+            int e = Integer.parseInt(st.nextToken()); //버스 도착 도시
+            int c = Integer.parseInt(st.nextToken()); //버스 비용
+            graph[s].add(new Bus(e, c));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+
+        int[] dist = new int[N+1]; //최소비용 저장 (0비움)
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start] = 0;
+        boolean[] v = new boolean[N+1]; //방문 여부 저장 (0비움)
+
+        PriorityQueue<Bus> pq = new PriorityQueue<>();
+        pq.offer(new Bus(start, 0));
+        while (!pq.isEmpty()) {
+            Bus now = pq.poll();
+            if(v[now.e]) continue;
+            v[now.e] = true;
+            for(Bus next : graph[now.e]) {
+                dist[next.e] = Math.min(dist[next.e], dist[now.e]+ next.c);
+                pq.offer(new Bus(next.e, dist[next.e]));
+                //System.out.println(next.e +" /  "+ next.c+" : "+Arrays.toString(dist));
+            }
+        }
+
+        System.out.println(dist[end]);
+    }
+}
